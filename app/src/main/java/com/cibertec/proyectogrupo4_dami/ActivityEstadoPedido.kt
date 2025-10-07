092bbf1f57076029d681d6ac2b4576f24fc7b06d
leomf-dev <leonelmelendezfalcon@gmail.com>
2025-10-07 12:52:14 -0500

    se agrega chat con el repartidor

--- app/src/main/java/com/cibertec/proyectogrupo4_dami/ActivityEstadoPedido.kt
+++ app/src/main/java/com/cibertec/proyectogrupo4_dami/ActivityEstadoPedido.kt
@@ -1,20 +1,56 @@
 package com.cibertec.proyectogrupo4_dami
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ActivityEstadoPedido : AppCompatActivity() {
+    private lateinit var btnSoporte : Button
+    private lateinit var btnContacto : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_estado_pedido)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        btnSoporte = findViewById(R.id.btnSoporte)
+        btnContacto = findViewById(R.id.btnContactarRepartidor)
+
+        btnSoporte.setOnClickListener {
+            val url = "https://wa.me/51987654321?text=Hola%2C+necesito+ayuda+con+mi+pedido"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+        btnContacto.setOnClickListener {
+            cambioAct(ContactoRepartidorActivity::class.java)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ActivityEstadoPedido)) { v, insets ->
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                maxOf(systemBars.bottom, imeInsets.bottom)
+            )
             insets
         }
+    }
+
+    fun cambioAct(activityDestino : Class<out Activity>){
+        val intent = Intent(this, activityDestino)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/cibertec/proyectogrupo4_dami/MainActivity.kt
+++ app/src/main/java/com/cibertec/proyectogrupo4_dami/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.cibertec.proyectogrupo4_dami
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnCambioAct : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnCambioAct = findViewById(R.id.btnCambioAct)
+
+        btnCambioAct.setOnClickListener {
+            cambioAct(ActivityEstadoPedido::class.java)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun cambioAct(activityDestino : Class<out Activity>){
+        val intent = Intent(this, activityDestino)
+        startActivity(intent)
     }
 }
